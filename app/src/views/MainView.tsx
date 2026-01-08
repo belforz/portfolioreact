ee30a96cb9abef67caf992a5887cf3f3b964b136
belforz <macedobeiramar@hotmail.com>
2026-01-08 20:12:34 -0300

    add: foot

--- app/src/views/MainView.tsx
+++ app/src/views/MainView.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import SplashAnimation from "../components/splash";
 import { About } from "../components/about";
 import { NavBar } from "../components/cards/NavBar";
@@ -13,6 +13,7 @@ import { Experience } from "../components/workcard";
 import { Project } from "../components/project";
 import { Skills } from "../components/skills";
 import { ChatMiniLeandro } from "../components/chatbot";
+import { Footer } from "../components/footer";
 
 export default function MainView() {
   const [showSplash, setShowSplash] = useState(portfolio.splashScreen);
@@ -39,19 +40,23 @@ export default function MainView() {
   }
 
   return (
-    <div className={`min-h-screen overscroll-contain bg-${darkModeActive ? 'black' : 'white'} ${darkModeActive ? 'text-white' : 'text-slate-700'} text-base md:text-xl`}>
+    <div
+      className={`min-h-screen overscroll-contain bg-${darkModeActive ? "black" : "white"} ${darkModeActive ? "text-white" : "text-slate-700"} text-base md:text-xl`}
+    >
       <NavBar showTransition={portfolio.transitions.active} />
 
-      <div
-        className="min-h-full !mx-10" 
-      >
+      <div className="min-h-full !mx-10">
         <div className="flex flex-col md:grid md:grid-cols-6">
           <div className="col-span-1 flex-initial relative mx-auto md:mx-0 order-2 md:order-none">
             <ul className="md:fixed md:bottom-20 md:left:0 md:right:0 mb-7 flex items-center space-x-8 gap-8 md:flex-col md:space-y-4 md:space-x-0">
               <li className="transition-all motion-reduce:transition-none duration-500">
                 <a href={portfolio.socialMediaLinks.github} target="_blank">
                   <img
-                    src={darkModeActive ? "assets/github-copilot-branco.png" : "assets/github-icon-novo.svg"}
+                    src={
+                      darkModeActive
+                        ? "assets/github-copilot-branco.png"
+                        : "assets/github-icon-novo.svg"
+                    }
                     className="h-8 w-8 md:h-12 md:w-12 transition ease-out hover:-translate-y-1 motion-reduce:hover:translate-y-0 duration-300"
                     style={{ fill: "currentColor" }}
                   />
@@ -61,7 +66,7 @@ export default function MainView() {
                 <a href={portfolio.socialMediaLinks.linkedin} target="_blank">
                   <Icon
                     icon={faLinkedin}
-                    size={'2x'}
+                    size={"2x"}
                     color={darkModeActive ? "white" : "black"}
                     className="transition ease-out hover:-translate-y-1 motion-reduce:hover:translate-y-0 duration-300"
                   />
@@ -74,7 +79,7 @@ export default function MainView() {
                 >
                   <Icon
                     icon={faCamera}
-                    size={'2x'}
+                    size={"2x"}
                     color={darkModeActive ? "white" : "black"}
                     className="transition ease-out hover:-translate-y-1 motion-reduce:hover:translate-y-0 duration-300"
                   />
@@ -85,47 +90,47 @@ export default function MainView() {
 
           <div className="col-span-4 flex-1 flex-col order-1 md:order-none ">
             <div className="container mx-auto max-w-full 2xl:max-w-6xl">
+              <Landing
+                content={portfolio.greeting}
+                transitions={portfolio.transitions}
+              />
 
-            <Landing
-              content={portfolio.greeting}
-              transitions={portfolio.transitions}
-            />
+              <About
+                content={{
+                  autobiography: portfolio.about.autobiography,
+                  techStack: portfolio.about.techStack,
+                  photo1Link: portfolio.about.photo1Link,
+                  photo2Link: portfolio.about.photo2Link,
+                }}
+                transitions={portfolio.transitions}
+              />
+              <Experience
+                content={{ experiences: portfolio.experiences }}
+                transitions={portfolio.transitions}
+              />
+              <Skills
+                content={{ skills: portfolio.skills }}
+                transitions={portfolio.transitions}
+              />
 
-            <About
-              content={{
-                autobiography: portfolio.about.autobiography,
-                techStack: portfolio.about.techStack,
-                photo1Link: portfolio.about.photo1Link,
-                photo2Link: portfolio.about.photo2Link,
-              }}
-              transitions={portfolio.transitions}
-            />
-            <Experience
-              content={{ experiences: portfolio.experiences }}
-              transitions={portfolio.transitions}
-            />
-            <Skills
-              content={{ skills: portfolio.skills }}
-              transitions={portfolio.transitions}
-            />
-
-            <Project 
-              content={{ works: portfolio.works}}
-              transitions={portfolio.transitions}
-            />
-            <Contact
-              content={portfolio.contact}
-              transitions={portfolio.transitions}
-            />
-            <ChatMiniLeandro
-        visible={popUpVisible}
-        onUpdateVisible={setPopUpVisible}
-      />
+              <Project
+                content={{ works: portfolio.works }}
+                transitions={portfolio.transitions}
+              />
+              <Contact
+                content={portfolio.contact}
+                transitions={portfolio.transitions}
+              />
+              <ChatMiniLeandro
+                visible={popUpVisible}
+                onUpdateVisible={setPopUpVisible}
+              />
+              <Footer />
+          
             </div>
           </div>
         </div>
       </div>
-      </div>
-    
+    </div>
   );
 }
